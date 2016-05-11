022645ac2eac6a8064330d8fb7433bf99ae5c2f0
unknown <cormatth@OA-S142-08.shernet.sheridancollege.ca>
2016-05-11 11:35:46 -0400

    Moved Prototype Build into main project

---
+++ Dream Catchers/Assets/_Game/Scripts/Managers/ManipulationManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManipulationManager : Singleton<ManipulationManager>
+{
+    public enum WORLD_STATE
+    {
+        DREAM = 0,
+        NIGHTMARE = 1
+    }
+     
+    public WORLD_STATE currentWorldState;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentWorldState = WORLD_STATE.DREAM;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentWorldState = (currentWorldState == WORLD_STATE.DREAM) ? WORLD_STATE.NIGHTMARE : WORLD_STATE.DREAM;
+        }
+
+    }
+
+}
---
+++ Dream Catchers/Assets/_Game/Scripts/WorldManipulation/ManipulationScript.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManipulationScript : MonoBehaviour
+{
+
+    public enum CHANGE_TYPE
+    {
+        NONE = 0,
+        TEXTURE = 1,
+        MESH = 2,
+        ANIMATION = 3,
+        PLATFORM = 4
+    }
+
+    public ManipulationManager.WORLD_STATE currentObjectState;
+    public CHANGE_TYPE objectChangeType;
+
+    public Texture dreamTexture;
+    public Texture nightmareTexture;
+
+    public Mesh dreamMesh;
+    public Mesh nightmareMesh;
+
+    public bool toggleCollisionWithMesh;
+    public Collider dreamCollider;
+    public Collider nightmareCollider;
+
+    public bool toggleAnimationWithMesh;
+    public AnimatorOverrideController dreamOverride;
+    public AnimatorOverrideController nightmareOveride;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentObjectState = ManipulationManager.WORLD_STATE.DREAM;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (ManipulationManager.Instance.currentWorldState != currentObjectState && objectChangeType != CHANGE_TYPE.NONE)
+        {
+            changeState(ManipulationManager.Instance.currentWorldState);
+        }
+    }
+
+    void changeState(ManipulationManager.WORLD_STATE state)
+    {
+        currentObjectState = state;
+        switch (objectChangeType)
+        {
+            case CHANGE_TYPE.TEXTURE:
+                {
+                    gameObject.GetComponent<Renderer>().material.mainTexture = (state == ManipulationManager.WORLD_STATE.DREAM) ? dreamTexture : nightmareTexture;
+                    break;
+                }
+            case CHANGE_TYPE.MESH:
+                {
+                    gameObject.GetComponent<MeshFilter>().mesh = (state == ManipulationManager.WORLD_STATE.DREAM) ? dreamMesh : nightmareMesh;
+                    if (toggleCollisionWithMesh)
+                    {
+                        if (state == ManipulationManager.WORLD_STATE.DREAM)
+                        {
+                            dreamCollider.enabled = true;
+                            nightmareCollider.enabled = false;
+                        }
+                        else
+                        {
+                            nightmareCollider.enabled = true;
+                            dreamCollider.enabled = false;
+                        }
+                    }
+                    break;
+                }
+            case CHANGE_TYPE.ANIMATION:
+                {
+                    gameObject.GetComponent<Animator>().runtimeAnimatorController = (state == ManipulationManager.WORLD_STATE.DREAM) ? dreamOverride : nightmareOveride;
+                    break;
+                }
+            case CHANGE_TYPE.PLATFORM:
+                {
+                    if (state == ManipulationManager.WORLD_STATE.DREAM)
+                    {
+                        dreamCollider.enabled = true;
+                        gameObject.GetComponent<ParticleSystem>().Stop();
+                        gameObject.GetComponent<MeshFilter>().mesh = dreamMesh;
+
+                    }
+                    else
+                    {
+                        dreamCollider.enabled = false;
+                        gameObject.GetComponent<ParticleSystem>().Play();
+                        gameObject.GetComponent<MeshFilter>().mesh = null;
+                    }
+
+                    break;
+                }
+        }
+    }
+}
