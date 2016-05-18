168ab40b2edcbf0f5cc61642b7a44bc63247e749
korkesh <matt.j.correia@gmail.com>
2016-05-18 13:27:24 -0400

    Imported Tweening Package; Reorganized existing code base

--- Dream Catchers/Assets/Editor/ManipulationInspector.cs
+++ Dream Catchers/Assets/Editor/ManipulationInspector.cs
@@ -1,149 +1,149 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEditor;
-using UnityEditor.AnimatedValues;
+﻿//using UnityEngine;
+//using System.Collections;
+//using UnityEditor;
+//using UnityEditor.AnimatedValues;
 
-[CustomEditor(typeof(ManipulationScript))]
-public class ManipulationInspector : Editor {
+//[CustomEditor(typeof(ManipulationScript))]
+//public class ManipulationInspector : Editor {
 
-    AnimBool m_ShowAppearenceFields;
-    bool appearenceBtn = false;
+//    AnimBool m_ShowAppearenceFields;
+//    bool appearenceBtn = false;
 
-    AnimBool m_ShowPhysicsFields;
-    bool physicsBtn = false;
+//    AnimBool m_ShowPhysicsFields;
+//    bool physicsBtn = false;
 
-    int platformIndex = 0;
+//    int platformIndex = 0;
 
-    ManipulationScript manipScript;
+//    ManipulationScript manipScript;
 
-    void Awake()
-    {
-        manipScript = (ManipulationScript)target;
+//    void Awake()
+//    {
+//        manipScript = (ManipulationScript)target;
 
-        m_ShowAppearenceFields = new AnimBool(false);
-        m_ShowAppearenceFields.valueChanged.AddListener(Repaint);
+//        m_ShowAppearenceFields = new AnimBool(false);
+//        m_ShowAppearenceFields.valueChanged.AddListener(Repaint);
 
-        m_ShowPhysicsFields = new AnimBool(false);
-        m_ShowPhysicsFields.valueChanged.AddListener(Repaint);
-    }
+//        m_ShowPhysicsFields = new AnimBool(false);
+//        m_ShowPhysicsFields.valueChanged.AddListener(Repaint);
+//    }
 
-    public override void OnInspectorGUI()
-    {
-        EditorGUILayout.LabelField ("Current State:     " + manipScript.currentObjectState.ToString(), EditorStyles.boldLabel);
-        EditorGUILayout.LabelField(" ");
+//    public override void OnInspectorGUI()
+//    {
+//        EditorGUILayout.LabelField ("Current State:     " + manipScript.currentObjectState.ToString(), EditorStyles.boldLabel);
+//        EditorGUILayout.LabelField(" ");
 
-        // Appearence Changes
-        appearenceBtn = EditorGUILayout.Toggle("Appearence Change", appearenceBtn);
-        if (appearenceBtn)
-        {
-            m_ShowAppearenceFields.value = true;
-        }
-        else
-        {
-            m_ShowAppearenceFields.value = false;
-        }
+//        // Appearence Changes
+//        appearenceBtn = EditorGUILayout.Toggle("Appearence Change", appearenceBtn);
+//        if (appearenceBtn)
+//        {
+//            m_ShowAppearenceFields.value = true;
+//        }
+//        else
+//        {
+//            m_ShowAppearenceFields.value = false;
+//        }
 
-        if (EditorGUILayout.BeginFadeGroup(m_ShowAppearenceFields.faded))
-        {
+//        if (EditorGUILayout.BeginFadeGroup(m_ShowAppearenceFields.faded))
+//        {
 
-            EditorGUI.indentLevel++;
+//            EditorGUI.indentLevel++;
 
-            EditorGUILayout.LabelField("Texture", EditorStyles.boldLabel);
-            EditorGUI.indentLevel++;
-            manipScript.dreamTexture = (Texture)EditorGUI.ObjectField(GUILayoutUtility.GetRect(15, 15, "TextField"), "Dream", manipScript.dreamTexture, typeof(Texture), true);
-            manipScript.nightmareTexture = (Texture)EditorGUI.ObjectField(GUILayoutUtility.GetRect(15, 15, "TextField"), "Nightmare", manipScript.nightmareTexture, typeof(Texture), true);
-            EditorGUI.indentLevel--;
+//            EditorGUILayout.LabelField("Texture", EditorStyles.boldLabel);
+//            EditorGUI.indentLevel++;
+//            manipScript.dreamTexture = (Texture)EditorGUI.ObjectField(GUILayoutUtility.GetRect(15, 15, "TextField"), "Dream", manipScript.dreamTexture, typeof(Texture), true);
+//            manipScript.nightmareTexture = (Texture)EditorGUI.ObjectField(GUILayoutUtility.GetRect(15, 15, "TextField"), "Nightmare", manipScript.nightmareTexture, typeof(Texture), true);
+//            EditorGUI.indentLevel--;
 
-            EditorGUILayout.LabelField(" ");
-            EditorGUILayout.LabelField("Mesh", EditorStyles.boldLabel);
-            EditorGUI.indentLevel++;
-            manipScript.dreamMesh = (Mesh)EditorGUI.ObjectField(GUILayoutUtility.GetRect(15, 15, "TextField"), "Dream", manipScript.dreamMesh, typeof(Mesh), true);
-            manipScript.nightmareMesh = (Mesh)EditorGUI.ObjectField(GUILayoutUtility.GetRect(15, 15, "TextField"), "Nightmare", manipScript.nightmareMesh, typeof(Mesh), true);
-            EditorGUI.indentLevel--;
-
-
-            EditorGUI.indentLevel--;
-        }
-
-        EditorGUILayout.EndFadeGroup();
-
-        // Physics Changes
-        EditorGUILayout.LabelField(" ");
-        physicsBtn = EditorGUILayout.Toggle("Physics Change", physicsBtn);
-        if (physicsBtn)
-        {
-            m_ShowPhysicsFields.value = true;
-        }
-        else
-        {
-            m_ShowPhysicsFields.value = false;
-        }
-
-        if (EditorGUILayout.BeginFadeGroup(m_ShowPhysicsFields.faded))
-        {
-
-            EditorGUI.indentLevel++;
-
-            EditorGUILayout.LabelField("Colliders", EditorStyles.boldLabel);
-            EditorGUI.indentLevel++;
-            manipScript.dreamCollider = (Collider)EditorGUI.ObjectField(GUILayoutUtility.GetRect(15, 15, "TextField"), "Dream", manipScript.dreamCollider, typeof(Collider), true);
-            manipScript.nightmareCollider = (Collider)EditorGUI.ObjectField(GUILayoutUtility.GetRect(15, 15, "TextField"), "Nightmare", manipScript.nightmareCollider, typeof(Collider), true);
-            EditorGUI.indentLevel--;
-        }
-
-        EditorGUILayout.EndFadeGroup();
-
-        // Misc Properties
-        EditorGUILayout.LabelField(" ");
-        EditorGUILayout.LabelField("Misc. Properties", EditorStyles.boldLabel);
-
-        // Platform Types
-        GUIContent platformLabel = new GUIContent("No Platform");
-        GUIContent platformDreamLabel = new GUIContent("Make Dream Platform", "Requires Dream Mesh and Dream Collider");
-        GUIContent platformNightmareLabel = new GUIContent("Make Nightmare Platform", "Requires Nightmare Mesh and Nightmare Collider");
-        GUIContent[] platformItems = { platformLabel, platformDreamLabel, platformNightmareLabel };
-
-        platformIndex = ToggleList(platformIndex, platformItems);
-        if(platformIndex == 1)
-        {
-            manipScript.isDreamPlatform = true;
-            manipScript.isNightmarePlatform = false;
-        }
-        else if (platformIndex == 2)
-        {
-            manipScript.isDreamPlatform = false;
-            manipScript.isNightmarePlatform = true;
-        }
-        else
-        {
-            manipScript.isDreamPlatform = false;
-            manipScript.isNightmarePlatform = false;
-        }
-        //DrawDefaultInspector();
+//            EditorGUILayout.LabelField(" ");
+//            EditorGUILayout.LabelField("Mesh", EditorStyles.boldLabel);
+//            EditorGUI.indentLevel++;
+//            manipScript.dreamMesh = (Mesh)EditorGUI.ObjectField(GUILayoutUtility.GetRect(15, 15, "TextField"), "Dream", manipScript.dreamMesh, typeof(Mesh), true);
+//            manipScript.nightmareMesh = (Mesh)EditorGUI.ObjectField(GUILayoutUtility.GetRect(15, 15, "TextField"), "Nightmare", manipScript.nightmareMesh, typeof(Mesh), true);
+//            EditorGUI.indentLevel--;
 
 
-    }
+//            EditorGUI.indentLevel--;
+//        }
 
-    /// <summary>
-    /// Displays a vertical list of toggles and returns the index of the selected item.
-    /// </summary>
-    public static int ToggleList(int selected, GUIContent[] items)
-    {
-        // Keep the selected index within the bounds of the items array
-        selected = selected < 0 ? 0 : selected >= items.Length ? items.Length - 1 : selected;
+//        EditorGUILayout.EndFadeGroup();
 
-        GUILayout.BeginVertical();
-        for (int i = 0; i < items.Length; i++)
-        {
-            // Display toggle. Get if toggle changed.
-            bool change = GUILayout.Toggle(selected == i, items[i]);
-            // If changed, set selected to current index.
-            if (change)
-                selected = i;
-        }
-        GUILayout.EndVertical();
+//        // Physics Changes
+//        EditorGUILayout.LabelField(" ");
+//        physicsBtn = EditorGUILayout.Toggle("Physics Change", physicsBtn);
+//        if (physicsBtn)
+//        {
+//            m_ShowPhysicsFields.value = true;
+//        }
+//        else
+//        {
+//            m_ShowPhysicsFields.value = false;
+//        }
 
-        // Return the currently selected item's index
-        return selected;
-    }
-}
+//        if (EditorGUILayout.BeginFadeGroup(m_ShowPhysicsFields.faded))
+//        {
+
+//            EditorGUI.indentLevel++;
+
+//            EditorGUILayout.LabelField("Colliders", EditorStyles.boldLabel);
+//            EditorGUI.indentLevel++;
+//            manipScript.dreamCollider = (Collider)EditorGUI.ObjectField(GUILayoutUtility.GetRect(15, 15, "TextField"), "Dream", manipScript.dreamCollider, typeof(Collider), true);
+//            manipScript.nightmareCollider = (Collider)EditorGUI.ObjectField(GUILayoutUtility.GetRect(15, 15, "TextField"), "Nightmare", manipScript.nightmareCollider, typeof(Collider), true);
+//            EditorGUI.indentLevel--;
+//        }
+
+//        EditorGUILayout.EndFadeGroup();
+
+//        // Misc Properties
+//        EditorGUILayout.LabelField(" ");
+//        EditorGUILayout.LabelField("Misc. Properties", EditorStyles.boldLabel);
+
+//        // Platform Types
+//        GUIContent platformLabel = new GUIContent("No Platform");
+//        GUIContent platformDreamLabel = new GUIContent("Make Dream Platform", "Requires Dream Mesh and Dream Collider");
+//        GUIContent platformNightmareLabel = new GUIContent("Make Nightmare Platform", "Requires Nightmare Mesh and Nightmare Collider");
+//        GUIContent[] platformItems = { platformLabel, platformDreamLabel, platformNightmareLabel };
+
+//        platformIndex = ToggleList(platformIndex, platformItems);
+//        if(platformIndex == 1)
+//        {
+//            manipScript.isDreamPlatform = true;
+//            manipScript.isNightmarePlatform = false;
+//        }
+//        else if (platformIndex == 2)
+//        {
+//            manipScript.isDreamPlatform = false;
+//            manipScript.isNightmarePlatform = true;
+//        }
+//        else
+//        {
+//            manipScript.isDreamPlatform = false;
+//            manipScript.isNightmarePlatform = false;
+//        }
+//        //DrawDefaultInspector();
+
+
+//    }
+
+//    /// <summary>
+//    /// Displays a vertical list of toggles and returns the index of the selected item.
+//    /// </summary>
+//    public static int ToggleList(int selected, GUIContent[] items)
+//    {
+//        // Keep the selected index within the bounds of the items array
+//        selected = selected < 0 ? 0 : selected >= items.Length ? items.Length - 1 : selected;
+
+//        GUILayout.BeginVertical();
+//        for (int i = 0; i < items.Length; i++)
+//        {
+//            // Display toggle. Get if toggle changed.
+//            bool change = GUILayout.Toggle(selected == i, items[i]);
+//            // If changed, set selected to current index.
+//            if (change)
+//                selected = i;
+//        }
+//        GUILayout.EndVertical();
+
+//        // Return the currently selected item's index
+//        return selected;
+//    }
+//}
--- Dream Catchers/Assets/_Game/Scripts/WorldManipulation/ManipulationScript.cs
+++ Dream Catchers/Assets/_Game/Scripts/WorldManipulation/ManipulationScript.cs
@@ -3,41 +3,18 @@ using System.Collections;
 
 public class ManipulationScript : MonoBehaviour
 {
-    // The type of changes that can be applied by a state change
-    public enum CHANGE_TYPE
+    public enum MANIPULATION_TYPE
     {
-        NONE = 0,
-        TEXTURE = 1,
-        MESH = 2,
-        ANIMATION = 3,
-        PLATFORM = 4
+        POP_IN = 0,
+        SCALE = 1,
+        TRANSLATE = 2,
+        ROTATE = 3,
+        OTHER = 4
     }
 
     // Objects state and change type
     public ManipulationManager.WORLD_STATE currentObjectState;
-    public CHANGE_TYPE objectChangeType;
-
-    // Textures
-    public Texture dreamTexture;
-    public Texture nightmareTexture;
-
-    // Meshs
-    public Mesh dreamMesh;
-    public Mesh nightmareMesh;
-
-    // Colliders
-    public bool toggleCollisionWithMesh;
-    public Collider dreamCollider;
-    public Collider nightmareCollider;
-
-    // Animations
-    public bool toggleAnimationWithMesh;
-    public AnimatorOverrideController dreamOverride;
-    public AnimatorOverrideController nightmareOveride;
-
-    // Manipulation Properties
-    public bool isDreamPlatform = false;
-    public bool isNightmarePlatform = false;
+    public MANIPULATION_TYPE currentManipType;
 
     // Use this for initialization
     void Start()
@@ -56,82 +33,5 @@ public class ManipulationScript : MonoBehaviour
         }
     }
 
-    // Applies the change logic to the current object upon state change
-    void changeState(ManipulationManager.WORLD_STATE state)
-    {
-        currentObjectState = state;
-
-        changeTexture();
-        changeMesh();
-        changeCollider();
-
-        togglePlatform();     
-    }
-
-    void changeTexture()
-    {
-        if (dreamTexture && nightmareTexture)
-        {
-            gameObject.GetComponent<Renderer>().material.mainTexture = (currentObjectState == ManipulationManager.WORLD_STATE.DREAM) ? dreamTexture : nightmareTexture;
-        }
-    }
-
-    void changeMesh()
-    {
-        if (dreamMesh && nightmareMesh)
-        {
-            gameObject.GetComponent<MeshFilter>().mesh = (currentObjectState == ManipulationManager.WORLD_STATE.DREAM) ? dreamMesh : nightmareMesh;
-        }
-    }
-
-    void changeCollider()
-    {
-        if (dreamCollider && nightmareCollider)
-        {
-            if (currentObjectState == ManipulationManager.WORLD_STATE.DREAM)
-            {
-                dreamCollider.enabled = true;
-                nightmareCollider.enabled = false;
-            }
-            else
-            {
-                nightmareCollider.enabled = true;
-                dreamCollider.enabled = false;
-            }
-        }
-    }
-
-    void togglePlatform()
-    {
-        if(isDreamPlatform)
-        {
-            if (currentObjectState == ManipulationManager.WORLD_STATE.DREAM)
-            {
-                dreamCollider.enabled = true;
-                gameObject.GetComponent<MeshFilter>().mesh = dreamMesh;
-
-            }
-            else
-            {
-                dreamCollider.enabled = false;
-                nightmareCollider.enabled = false;
-                gameObject.GetComponent<MeshFilter>().mesh = null;
-            }
-        }
-        else if (isNightmarePlatform)
-        {
-            if (currentObjectState == ManipulationManager.WORLD_STATE.NIGHTMARE)
-            {
-                nightmareCollider.enabled = true;
-                gameObject.GetComponent<MeshFilter>().mesh = nightmareMesh;
-
-            }
-            else
-            {
-                dreamCollider.enabled = false;
-                nightmareCollider.enabled = false;
-                gameObject.GetComponent<MeshFilter>().mesh = null;
-            }
-        }
-    }
+    public virtual void changeState(ManipulationManager.WORLD_STATE state) { }
 }
