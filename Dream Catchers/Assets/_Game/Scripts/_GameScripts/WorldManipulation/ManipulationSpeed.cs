56b9adc5f20466689a0272430d89d7ce6195b4fc
korkesh <matt.j.correia@gmail.com>
2016-05-20 15:47:00 -0400

    Added Friction Manipulation

---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/WorldManipulation/ManipulationSpeed.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class ManipulationSpeed : ManipulationScript
+{
+    public GameObject player;
+
+    public float slowSpeed;
+    public float increaseSpeed;
+
+    public bool slowInNightmare;
+    public bool slowInDream;
+
+    public bool quickenInNightmare;
+    public bool quickenInDream;
+
+    private float playerOriginalSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentManipType = MANIPULATION_TYPE.OTHER;
+
+        PlayerMachine controllerScript = player.GetComponent<PlayerMachine>();
+        playerOriginalSpeed = controllerScript.WalkSpeed;
+
+    }
+
+    public override void changeState(ManipulationManager.WORLD_STATE state)
+    {
+        currentObjectState = state;
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == player && 
+            ((slowInNightmare && currentObjectState == ManipulationManager.WORLD_STATE.NIGHTMARE)
+            || (slowInDream && currentObjectState == ManipulationManager.WORLD_STATE.DREAM)))
+        {
+            PlayerMachine controllerScript = player.GetComponent<PlayerMachine>();
+            controllerScript.WalkSpeed = slowSpeed;
+        }
+        else if (other.gameObject == player &&
+            ((quickenInNightmare && currentObjectState == ManipulationManager.WORLD_STATE.NIGHTMARE)
+            || (quickenInDream && currentObjectState == ManipulationManager.WORLD_STATE.DREAM)))
+        {
+            PlayerMachine controllerScript = player.GetComponent<PlayerMachine>();
+            controllerScript.WalkSpeed = increaseSpeed;
+        }
+        else
+        {
+            PlayerMachine controllerScript = player.GetComponent<PlayerMachine>();
+            controllerScript.WalkSpeed = playerOriginalSpeed;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            PlayerMachine controllerScript = player.GetComponent<PlayerMachine>();
+            controllerScript.WalkSpeed = playerOriginalSpeed;
+        }
+    }
+}
