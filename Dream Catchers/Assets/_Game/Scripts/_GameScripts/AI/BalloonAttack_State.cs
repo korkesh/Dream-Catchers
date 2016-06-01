c47e800693ed58dd7bb64322646ed5b026b4d60f
unknown <gallalex@OA-S142-01.shernet.sheridancollege.ca>
2016-06-01 13:14:32 -0400

    more AI Fixes for balloon animal

--- Dream Catchers/Assets/_Game/Scripts/_GameScripts/AI/BalloonAttack_State.cs
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/AI/BalloonAttack_State.cs
@@ -5,6 +5,7 @@ public class BalloonAttack_State : BaseState {
 
     public float radiusTolaunchAttack;
     public bool IsAttacking;
+    bool IsJumping;
     public float waitTillNextAttack;
     bool wait;
     float waittime;
@@ -12,10 +13,13 @@ public class BalloonAttack_State : BaseState {
     public float AttackDuration;
     float attackTIme;
     GameObject Player;
-    GameObject ReturnSpot;
+    public GameObject ReturnSpot;
+    public float AllowableDistance;
     float speed;
     public float attackSpeed;
     public DamageDealer damage;
+    public Rigidbody rigidB;
+    public float shootAngle;
 
     void Awake()
     {
@@ -26,7 +30,9 @@ public class BalloonAttack_State : BaseState {
         wait = false;
         waittime = waitTillNextAttack;
         attackTIme = AttackDuration;
+        IsJumping = false;
         speed = NavAgent.speed;
+        rigidB = GetComponent<Rigidbody>();
     }
 
 
@@ -42,60 +48,112 @@ public class BalloonAttack_State : BaseState {
         {
             fsm.changeState("FloatToStart");
 
-        }else if (wait == true)
+        }
+        else if (Vector3.Distance(transform.position, ReturnSpot.transform.position) >= AllowableDistance && Vector3.Distance(ReturnSpot.transform.position, Player.transform.position) < AllowableDistance)
         {
-            waittime -= Time.deltaTime;
-            if(waittime <= 0)
-            {
-                wait = false;
-                IsAttacking = true;
-                waittime = waitTillNextAttack;
-            }
+            NavAgent.enabled = true;
+            IsJumping = false;
+            IsAttacking = false;
+            NavAgent.SetDestination(ReturnSpot.transform.position);
+        }
+        //else if (wait == true)
+        //{
+        //    waittime -= Time.deltaTime;
+        //    if(waittime <= 0)
+        //    {
+        //        wait = false;
+        //        IsAttacking = true;
+        //        waittime = waitTillNextAttack;
+        //    }
 
-        }else if(IsAttacking == false)
+        //}
+        else if(IsAttacking == false)
         {
-           
-            if(Vector3.Distance(transform.position,Player.transform.position) <= radiusTolaunchAttack)
+
+            if (Vector3.Distance(transform.position, Player.transform.position) <= radiusTolaunchAttack && Vector3.Distance(ReturnSpot.transform.position, Player.transform.position) < AllowableDistance)
             {
                 IsAttacking = true;
             }
             else
             {
-                NavAgent.SetDestination(Player.transform.position);
+               // NavAgent.SetDestination(Player.transform.position);
+                NavAgent.SetDestination(ReturnSpot.transform.position);
             }
         }
         else
         {
-            attackTIme -= Time.deltaTime;
-            if(attackTIme >= 0)
+            if(IsJumping == false && Vector3.Distance(transform.position, Player.transform.position) >= (radiusTolaunchAttack-(radiusTolaunchAttack-2)))
             {
-                NavAgent.speed = attackSpeed;
-                NavAgent.SetDestination(Player.transform.position);
+                NavAgent.enabled = false;
+                rigidB.velocity = Jump(Player.transform, shootAngle);
+                IsJumping = true;
+
             }
-            else
+            else if (Vector3.Distance(transform.position, Player.transform.position) <= (radiusTolaunchAttack - (radiusTolaunchAttack - 2)) && rigidB.velocity.y == 0)
             {
-                attackTIme = AttackDuration;
-                wait = true;
-                NavAgent.speed = speed;
+                NavAgent.enabled = true;
+                IsJumping = false;
+                NavAgent.SetDestination(ReturnSpot.transform.position);
+
+            }else if (rigidB.velocity.y == 0)
+            {
+
                 IsAttacking = false;
+                IsJumping = false;
+                NavAgent.enabled = true;
+
             }
+
+            //attackTIme -= Time.deltaTime;
+            //if(attackTIme >= 0)
+            //{
+            //    NavAgent.speed = attackSpeed;
+            //    NavAgent.SetDestination(Player.transform.position);
+            //}
+            //else
+            //{
+            //    attackTIme = AttackDuration;
+            //    wait = true;
+            //    NavAgent.speed = speed;
+            //    IsAttacking = false;
+            //}
         }
     }
 
     public override void Exit()
     {
         NavAgent.enabled = false;
+        wait = false;
+        waittime = waitTillNextAttack;
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player" && wait == false)
+        if (collision.gameObject.tag == "Player")
         {
             attackTIme = AttackDuration;
             wait = true;
             NavAgent.speed = speed;
             IsAttacking = false;
             damage.DealDamage();
+             //&& wait == false
         }
     }
+
+    Vector3 Jump( Transform target,  float angle)  
+    {
+        Vector3 dir = target.position - transform.position;  // get target direction
+        float h = dir.y;  // get height difference
+        dir.y = 0;  // retain only the horizontal direction
+        float dist = dir.magnitude ;  // get horizontal distance
+        float a = angle * Mathf.Deg2Rad;  // convert angle to radians
+        dir.y = dist * Mathf.Tan(a);  // set dir to the elevation angle
+        dist += h / Mathf.Tan(a);  // correct for small height differences
+        // calculate the velocity magnitude
+        float sin = Mathf.Sin(2 * a);
+        float div = dist * Physics.gravity.magnitude / sin;
+        float vel = Mathf.Sqrt(div);
+        Debug.Log(vel * dir.normalized);
+        return vel * dir.normalized;
+ }
 }
---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/AI/BalloonChase_State.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalloonChase_State : BaseState {
+
+    public float attackRadius;
+    public float MaxAllowedDist;
+    GameObject Player;
+    public GameObject ReturnSpot;
+    public NavMeshAgent NavAgent;
+
+    void Awake()
+    {
+        //get fsm
+        fsm = this.gameObject.GetComponent<FSM>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+    public override void Enter()
+    {
+        NavAgent.enabled = true;
+    }
+
+    public override void Execute()
+    {
+        if (ManipulationManager.instance.currentWorldState == ManipulationManager.WORLD_STATE.DREAM)
+        {
+            fsm.changeState("FloatToStart");
+
+        }
+        else if (Vector3.Distance(transform.position, ReturnSpot.transform.position) >= MaxAllowedDist && Vector3.Distance(Player.transform.position, ReturnSpot.transform.position) >= MaxAllowedDist)
+        {
+            //|| Vector3.Distance(ReturnSpot.transform.position, Player.transform.position) >= MaxAllowedDist
+            fsm.changeState("ReturnSpot");
+        }
+        else if (Vector3.Distance(transform.position, Player.transform.position) <= attackRadius)
+        {
+            fsm.changeState("Attack");
+        }
+        else
+        {
+            NavAgent.SetDestination(Player.transform.position);
+        }
+
+    }
+
+    public override void Exit()
+    {
+        NavAgent.enabled = false;
+    }
+}
