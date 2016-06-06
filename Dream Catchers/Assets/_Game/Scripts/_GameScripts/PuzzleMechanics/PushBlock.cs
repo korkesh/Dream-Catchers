437a6eb41a07fc167667d065b0aec7a5eb693df9
Jesi Mcmulkin <mcmulkij@shernet.sheridancollege.ca>
2016-06-06 11:26:24 -0400

    Implemented greybox area, updated all collectibles

--- Dream Catchers/Assets/_Game/Scripts/_GameScripts/PuzzleMechanics/PushBlock.cs
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/PuzzleMechanics/PushBlock.cs
@@ -4,37 +4,24 @@ using DG.Tweening;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PushBlock : MonoBehaviour {
-
     public float pushPower = 2.0f;
     public float pushTime = 0.5f;
-
-    public bool playerPush;
-
-    
-    void OnCollisionEnter(Collision collision)
-    {
-        // Only allow push from player and when not animating
-        if (collision.gameObject.tag == "Player" && !DOTween.IsTweening(transform) && playerPush)
-        {
-            Push();
-        }
-
-    }
 
     /// <summary>
     /// Pushes a box in the direction the character is facing by the pushpower amount over pushtime seconds.
     /// The box will not be pushed if the character comes at it from an angle.
     /// </summary>
-    public void Push()
+    void OnCollisionEnter(Collision collision)
     {
-        if (!DOTween.IsTweening(transform))
+        // Only allow push from player and when not animating
+        if (collision.gameObject.tag == "Player" && !DOTween.IsTweening(transform))
         {
             // The direction the character is facing when colliding with the box
-            Vector3 pushDir = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMachine>().facing.normalized;
+            Vector3 pushDir = collision.gameObject.GetComponent<PlayerMachine>().moveDirection.normalized;
 
             // If the character is not head on do not push
             if (Mathf.Round(pushDir.x) != 0 && Mathf.Round(pushDir.z) != 0)
-            {
+            { 
                 return;
             }
 
@@ -43,6 +30,8 @@ public class PushBlock : MonoBehaviour {
 
             // The push animation
             transform.DOMove(pushTo, pushTime);
+
         }
+
     }
 }
