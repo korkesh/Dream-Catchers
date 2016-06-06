7036cca3d94d9a3f30708d406e486604880a622b
unknown <gallalex@OA-S142-01.shernet.sheridancollege.ca>
2016-06-06 11:51:38 -0400

    Collectible Spawner Code

---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/Collectibles/CollectibleSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CollectibleSpawner : MonoBehaviour {
+
+    public List<GameObject> Prefabs;
+    public float probabilityOfDrop;
+
+	// Use this for initialization
+	void Start () {
+	
+        if(probabilityOfDrop > 1)
+        {
+            probabilityOfDrop = probabilityOfDrop / 100;
+        }
+	}
+
+    //Item drop when the object is destroyed
+    void OnDestroy()
+    {
+        float rand;
+        for(int i = 0; i < Prefabs.Count; i++)
+        {
+            rand = Random.value;
+            if(rand < probabilityOfDrop)
+            {
+                Instantiate(Prefabs[i], this.transform.position, this.transform.rotation);
+            }
+           
+        }
+    }
+}
