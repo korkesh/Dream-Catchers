ff7e8f519e0fbe513c95922aae8f94b17f56657a
korkesh <matt.j.correia@gmail.com>
2016-06-06 20:45:35 -0400

    More fixes

---
+++ Dream Catchers/Assets/_Game/Scenes/_TestScenes/Weekly Builds/Week4 - PreAlpha/TempCameraSwap.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TempCameraSwap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void enableSmartCam()
+    {
+        Game_Manager.instance.enableSmartCam = this.gameObject.GetComponent<Toggle>().isOn;
+
+    }
+
+}
