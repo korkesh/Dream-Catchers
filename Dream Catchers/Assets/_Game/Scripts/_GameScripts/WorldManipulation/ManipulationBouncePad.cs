6ad5b3ea9c639a04a1926217479749b5a76fbd0b
korkesh <matt.j.correia@gmail.com>
2016-06-09 19:37:14 -0400

    Adjust jump heights on bounce pads

--- Dream Catchers/Assets/_Game/Scripts/_GameScripts/WorldManipulation/ManipulationBouncePad.cs
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/WorldManipulation/ManipulationBouncePad.cs
@@ -6,16 +6,27 @@ public class ManipulationBouncePad : ManipulationScript
 {
     public GameObject player; // The player to act upon
 
+    public float bounceMaxHeight;
+    public float bounceMinHeight;
+    public float bounceAcceleration;
+
     public bool bounceInDream;
     public bool bounceInNightmare;
 
     private bool bounceObject;
+    private float originalMaxHeight;
+    private float originalMinHeight;
+    private float originalAcceleration;
 
     // Use this for initialization
     void Start()
     {
         // Set the default world state
         currentManipType = MANIPULATION_TYPE.OTHER;
+        player = GameObject.FindGameObjectWithTag("Player");
+        originalMaxHeight = player.GetComponent<PlayerMachine>().MaxJumpHeight;
+        originalMinHeight = player.GetComponent<PlayerMachine>().MinJumpHeight;
+        originalAcceleration = player.GetComponent<PlayerMachine>().JumpAcceleration;
 
         setBounce();
     }
@@ -43,12 +54,36 @@ public class ManipulationBouncePad : ManipulationScript
         }
     }
 
+    public void OnTriggerEnter(Collider other)
+    {
+        if (bounceObject && other.gameObject == player)
+        {
+            player.GetComponent<PlayerMachine>().MaxJumpHeight = bounceMaxHeight;
+            player.GetComponent<PlayerMachine>().MinJumpHeight = bounceMinHeight;
+            player.GetComponent<PlayerMachine>().JumpAcceleration = bounceAcceleration;
+        }
+    }
+
     public void OnTriggerStay(Collider other)
     {
         if(bounceObject && other.gameObject == player)
         {
+            player.GetComponent<PlayerMachine>().MaxJumpHeight = bounceMaxHeight;
+            player.GetComponent<PlayerMachine>().MinJumpHeight = bounceMinHeight;
+            player.GetComponent<PlayerMachine>().JumpAcceleration = bounceAcceleration;
+
             PlayerInputController inputScript = other.gameObject.GetComponent<PlayerInputController>();
             inputScript.toggleJump = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (bounceObject && other.gameObject == player)
+        {
+            player.GetComponent<PlayerMachine>().MaxJumpHeight = originalMaxHeight;
+            player.GetComponent<PlayerMachine>().MinJumpHeight = originalMinHeight;
+            player.GetComponent<PlayerMachine>().JumpAcceleration = originalAcceleration;
         }
     }
 
