c871c4e9563f14cdf441b0a5afdb774e48a6a92e
unknown <gallalex@OA-S142-01.shernet.sheridancollege.ca>
2016-06-10 15:28:38 -0400

    enemy spawn

---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/AI/EnemySpawn.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemySpawn : MonoBehaviour {
+
+    public List<GameObject> enemys;
+
+    void Awake()
+    {
+        for (int i = 0; i < enemys.Count; i++)
+        {
+            if (enemys[i] != null)
+            {
+                enemys[i].SetActive(false);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Fun");
+        if(other.gameObject.tag == "Player")
+        {
+            for (int i = 0; i < enemys.Count; i++)
+            {
+                if (enemys[i] != null)
+                {
+                    enemys[i].SetActive(true);
+                }
+            }
+        }
+       
+    }
+}
