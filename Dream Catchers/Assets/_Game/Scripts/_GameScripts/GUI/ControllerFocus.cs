a2326ca9eef48980a7fc10cb8bb4954589b053ae
korkesh <matt.j.correia@gmail.com>
2016-06-11 16:29:06 -0400

    Fixed mouse/controller focus

---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/GUI/ControllerFocus.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ControllerFocus : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(EventSystem.current.currentSelectedGameObject == null)
+         {
+            EventSystem.current.SetSelectedGameObject(EventSystem.current.firstSelectedGameObject);
+        }
+    }
+}
