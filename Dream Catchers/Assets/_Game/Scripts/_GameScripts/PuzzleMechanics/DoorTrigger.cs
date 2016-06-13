4b7403c6b432b658b7648d7b523ce95beaa26b30
korkesh <matt.j.correia@gmail.com>
2016-06-12 21:06:34 -0400

    Added models and door trigger

---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/PuzzleMechanics/DoorTrigger.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DoorTrigger : MonoBehaviour {
+
+    public enum TRIGGER_TYPE
+    {
+        ENEMY = 0
+    }
+
+    public TRIGGER_TYPE triggerType = TRIGGER_TYPE.ENEMY;
+
+    public List<GameObject> enemiesToKill;
+
+    public LowerDoor lowerDoorRef;
+
+    bool opened = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(opened)
+        {
+            return;
+        }
+
+        foreach(GameObject go in enemiesToKill)
+        {
+            if(go != null)
+            {
+                return;
+            }
+        }
+
+        lowerDoorRef.ActivateDoor();
+        opened = true;
+	}
+}
