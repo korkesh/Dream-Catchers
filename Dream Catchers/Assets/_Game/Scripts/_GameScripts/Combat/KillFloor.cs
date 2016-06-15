94818dcba20ed7e7937f32fdd6105169c18d0eea
korkesh <matt.j.correia@gmail.com>
2016-06-15 15:14:35 -0400

    Added working kill floor prefab

---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/Combat/KillFloor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillFloor : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Character_Manager.instance.takeDamage(Character_Manager.instance.maxHealth);
+        }
+    }
+}
