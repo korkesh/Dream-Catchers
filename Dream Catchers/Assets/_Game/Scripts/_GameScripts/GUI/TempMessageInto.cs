062c860a62a2613f8aefefd19412cefddddfc2b1
unknown <gallalex@OA-S142-01.shernet.sheridancollege.ca>
2016-06-15 15:42:19 -0400

    little intro info on new game... build settings have been changed

---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/GUI/TempMessageInto.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TempMessageInto : MonoBehaviour {
+
+    public string nextscene;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(Input.GetButtonDown("Jump") == true)
+        {
+            Game_Manager.instance.ChangeScene(nextscene);
+        }
+
+	}
+}
