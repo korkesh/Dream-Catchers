ff7a33c652e6b171472fb75eac9b9c3c3daad841
unknown <gallalex@OA-S142-01.shernet.sheridancollege.ca>
2016-06-22 16:22:53 -0400

    New AI stuff with Animation. Problem with rotation

---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/AI/BalloonAnimalRound2/BalloonBackup2.cs
@@ -0,0 +1,90 @@
+﻿//================================
+// Alex
+//  balloon animal backup after attack
+//================================
+using UnityEngine;
+using System.Collections;
+
+public class BalloonBackup2 : BaseState {
+
+    //================================
+    // Variables
+    //================================
+
+    Animator anim;
+    public float speed;
+    public float backUp;
+    Vector3 newPOs;
+    GameObject Player;
+
+
+    //================================
+    // Methods
+    //================================
+
+    //-----------------
+    // Initialization
+    //-----------------
+
+    void Awake()
+    {
+        //get fsm
+        fsm = this.gameObject.GetComponent<FSM>();
+        anim = this.gameObject.GetComponentInChildren<Animator>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+        if (anim == null)
+        {
+            Debug.Log("error no animator");
+        }
+    }
+
+
+    //-----------------
+    // FSM MEthods
+    //-----------------
+
+    public override void Enter()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("BalloonAnimalIdle") == false)
+        {
+            anim.SetTrigger("Idle");
+        }
+
+        newPOs = this.transform.position + (-1) * backUp * this.transform.forward;
+    }
+
+    public override void Execute()
+    {
+        if (ManipulationManager.instance.currentWorldState == ManipulationManager.WORLD_STATE.DREAM)
+        {
+            fsm.changeState("FloatToStart");
+
+        }else if (Vector3.Distance(this.transform.position,newPOs) >= 0.05)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, newPOs, step);
+
+        }
+        else if (Vector3.Distance(this.transform.position, newPOs) < 0.05)
+        {
+            fsm.changeState("Run");
+        }
+
+        WatchPlayer();
+    }
+
+    public override void Exit()
+    {
+
+    }
+
+    void WatchPlayer()
+    {
+        Vector3 lookDir = Player.transform.position;
+        lookDir.y = transform.position.y;
+        Vector3 targetDir = lookDir - transform.position;
+        float step = 3 * Time.deltaTime;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+        transform.rotation = Quaternion.LookRotation(newDir);
+    }
+}
---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/AI/BalloonAnimalRound2/BalloonDrop2.cs
@@ -0,0 +1,88 @@
+﻿//================================
+// Alex
+//  Balloon drop 2
+//================================
+
+using UnityEngine;
+using System.Collections;
+
+public class BalloonDrop2 : BaseState {
+
+    //================================
+    // Variables
+    //================================
+
+    Animator anim;
+    Rigidbody rigidB;
+    NavMeshAgent Navmesh;
+    bool hit;
+
+
+    //================================
+    // Methods
+    //================================
+
+    //-----------------
+    // Initialization
+    //-----------------
+
+
+    void Awake()
+    {
+        //get fsm
+        fsm = this.gameObject.GetComponent<FSM>();
+        rigidB = this.gameObject.GetComponent<Rigidbody>();
+        Navmesh = this.GetComponent<NavMeshAgent>();
+        anim = this.gameObject.GetComponentInChildren<Animator>();
+        if (anim == null)
+        {
+            Debug.Log("error no animator");
+        }
+    }
+
+    //-----------------
+    // FSM methods
+    //-----------------
+
+    public override void Enter()
+    {
+        rigidB.useGravity = true;
+        rigidB.velocity = Vector3.zero;
+        if (Navmesh != null)
+        {
+            Navmesh.enabled = false;
+        }
+        hit = false;
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("BalloonAnimalIdle") == false)
+        {
+            anim.SetTrigger("Idle");
+        }
+    }
+
+    public override void Execute()
+    {
+
+        if (ManipulationManager.instance.currentWorldState == ManipulationManager.WORLD_STATE.DREAM)
+        {
+            fsm.changeState("FloatToStart");
+
+        }
+        else if (rigidB.velocity.y == 0 && hit == true)
+        {
+            fsm.changeState("Run");
+        }
+    }
+
+    public override void Exit()
+    {
+        if (Navmesh != null)
+        {
+            Navmesh.enabled = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        hit = true;
+    }
+}
