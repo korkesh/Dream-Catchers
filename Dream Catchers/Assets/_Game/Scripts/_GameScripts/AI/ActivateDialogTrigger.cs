d641eab74d762a69a247e33b33933e546fd25331
unknown <gallalex@OA-S142-01.shernet.sheridancollege.ca>
2016-07-06 17:52:03 -0400

    started up the dialog.. need to run thru with jesi

---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/AI/ActivateDialogTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fungus;
+
+public class ActivateDialogTrigger : MonoBehaviour
+{
+
+    public Flowchart flC;
+    public string message;
+
+    void OnDestroy()
+    {
+        flC.SendFungusMessage(message);
+    }
+}
+     
---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/AI/EnterDEstroy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterDEstroy : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
