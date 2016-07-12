25d9260f0e480ffcbc036047318aeb82becdff75
Jesi Mcmulkin <mcmulkij@shernet.sheridancollege.ca>
2016-07-12 14:18:40 -0400

    Small changes to tutorial, fixed some spelling in textboxes. Couple additions to level one, should have a preliminary greybox done tomorrow

--- Dream Catchers/Assets/_Game/Scripts/_GameScripts/AI/GenericTrigger.cs
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/AI/GenericTrigger.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class GenericTrigger : MonoBehaviour {
 
-    public GameObject ObjectToTrigger; // The object which will trigger upon activation
+    public string ObjectToTriggerName; // The object which will trigger upon activation
     public string TriggerFunctionCall; // Method to trigger
 
     public void OnTriggerEnter(Collider other)
     {
+        GameObject ObjectToTrigger = GameObject.Find(ObjectToTriggerName);
+
         ObjectToTrigger.SendMessage(TriggerFunctionCall);
     }
 }
