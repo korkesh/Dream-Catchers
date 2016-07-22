00c9bb578c57097f6197f118e23ad20016985d4e
unknown <gallalex@OA-S142-01.shernet.sheridancollege.ca>
2016-07-22 17:48:25 -0400

    Rough boss fight. Missing blocks

--- Dream Catchers/Assets/_Game/Scripts/_GameScripts/AI/Boss/BallSpawner.cs
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/AI/Boss/BallSpawner.cs
@@ -43,7 +43,26 @@ public class BallSpawner : MonoBehaviour {
         }
         else
         {
+            if (currentAttack == null)
+            {
+                BossClown Bc = GameObject.FindObjectOfType<BossClown>();
+                if (Bc != null)
+                {
+                    if(Bc.RightHand.spawner.currentAttack == null && Bc.LeftHand.spawner.currentAttack == null)
+                    {
+                         if (Bc.RightHand.attack == HandScript.Mode.THROW)
+                         {
+                             Bc.RightHand.attack = HandScript.Mode.NONE;
+                         }
 
+                         if (Bc.LeftHand.attack == HandScript.Mode.THROW)
+                         {
+                             Bc.LeftHand.attack = HandScript.Mode.NONE;
+                         }
+                    }
+                    
+                }
+            }
         }
 	
 	}
---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/AI/Boss/StageThree.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageThree : Stages {
+
+    public bool RightHand;
+    int beginHealth;
+    public int healthDifference;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public override void Play()
+    {
+        if (Bc.Health <= (beginHealth - healthDifference))
+        {
+            Bc.currentStage = NextStage;
+        }
+
+        if(Bc.RightHand.attack == HandScript.Mode.NONE && Bc.LeftHand.attack == HandScript.Mode.NONE)
+        {
+            float rand = Random.Range(0, 30.0f);
+            if(rand < 1.0f)
+            {
+                if(RightHand == false)
+                {
+                    Bc.RightHand.ThrowBall();
+                }
+                else
+                {
+                    Bc.LeftHand.ThrowBall();
+                }
+
+            }else if(rand < 20)
+            {
+                if (RightHand == false)
+                {
+                    Bc.RightHand.Swipe();
+                }
+                else
+                {
+                    Bc.LeftHand.Swipe();
+                }
+            }
+            else
+            {
+                if (RightHand == false)
+                {
+                    Bc.RightHand.ChargeSmackDown();
+                }
+                else
+                {
+                    Bc.RightHand.ChargeSmackDown();
+                }
+            }
+
+            RightHand = !RightHand;
+        }
+
+       
+    }
+}
