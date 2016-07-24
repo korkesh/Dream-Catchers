4e48d54ea03e8894f16da311c6c10cccf5bd2ddf
korkesh <matt.j.correia@gmail.com>
2016-07-24 17:33:23 -0400

    Updated Translate to allow looping and pausing

--- Dream Catchers/Assets/_Game/Scripts/_GameScripts/WorldManipulation/ManipulationTranslate.cs
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/WorldManipulation/ManipulationTranslate.cs
@@ -16,13 +16,41 @@ public class ManipulationTranslate : ManipulationScript
 
     public float translateDuration;
 
+    public bool loop;
+    public float timeBetweenLoop;
+    public bool pauseInDream;
+    public bool pauseInNightmare;
+
+    Sequence mySequence;
+
     // Use this for initialization
     void Start()
     {
         // Set the default world state
         currentManipType = MANIPULATION_TYPE.TRANSLATE;
-
+        
         objectTransform = gameObject.transform; // By default grabs the transform of the attached object
+        
+        if(loop)
+        {
+            // Create new Sequence object
+            mySequence = DOTween.Sequence();
+
+            // Pause at first position
+            mySequence.Append(objectTransform.DOMove(translateDream, timeBetweenLoop));
+            
+            // Move To second position
+            mySequence.Append(objectTransform.DOMove(translateNightmare, translateDuration));
+
+            // Pause At second position
+            mySequence.Append(objectTransform.DOMove(translateNightmare, timeBetweenLoop));
+
+            // Move To first position
+            mySequence.Append(objectTransform.DOMove(translateDream, translateDuration));
+
+            // Loop sequence
+            mySequence.SetLoops(-1, LoopType.Restart);
+        }
     }
 
     public override void changeState(ManipulationManager.WORLD_STATE state)
@@ -32,11 +60,40 @@ public class ManipulationTranslate : ManipulationScript
         // Translate object to the given position over the given time duration
         if (currentObjectState == ManipulationManager.WORLD_STATE.DREAM)
         {
-            objectTransform.DOMove(translateDream, translateDuration);
+            if (loop && pauseInDream)
+            {
+                Debug.Log("Pause");
+                mySequence.TogglePause();
+            }
+            else if (loop)
+            {
+                Debug.Log("Play");
+                mySequence.TogglePause();
+            }
+            else
+            {
+                Debug.Log("Other");
+                objectTransform.DOMove(translateDream, translateDuration);
+            }
+
         }
         else
         {
-            objectTransform.DOMove(translateNightmare, translateDuration);
+            if (loop && pauseInNightmare)
+            {
+                Debug.Log("Pause");
+                mySequence.TogglePause();
+            }
+            else if (loop)
+            {
+                Debug.Log("Play");
+                mySequence.TogglePause();
+            }
+            else
+            {
+                Debug.Log("Other");
+                objectTransform.DOMove(translateNightmare, translateDuration);
+            }
         }
     }
 }
