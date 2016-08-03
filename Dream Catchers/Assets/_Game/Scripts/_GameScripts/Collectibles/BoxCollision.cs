6fe2b3de316db258315a640b41c64ed1adb63f1c
korkesh <matt.j.correia@gmail.com>
2016-08-03 17:21:10 -0400

    Fixed Boxes Falling on head

---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/Collectibles/BoxCollision.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoxCollision : MonoBehaviour {
+
+    public float groundHeight;
+    public List<GameObject> boxesToDestroy;
+    public GameObject colliderToDestroy;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        foreach(GameObject g in boxesToDestroy)
+        {
+            if(g != null)
+            {
+                return;
+            }
+        }
+
+        if (transform.position.y <= groundHeight)
+        {
+            colliderToDestroy.SetActive(false);
+        }
+    }
+}
---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/Collectibles/IgnoreColliders.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgnoreColliders : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Blocker")
+        {
+            Physics.IgnoreCollision(collision.collider, GetComponent<Collider>());
+        }
+
+    }
+}
