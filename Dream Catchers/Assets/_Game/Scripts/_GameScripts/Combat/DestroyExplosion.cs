45c174017816e12121918e6417b736aa55d172c4
korkesh <matt.j.correia@gmail.com>
2016-08-10 12:05:51 -0400

    Exploding Boxes

---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/Combat/DestroyExplosion.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyExplosion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Physics.IgnoreCollision(GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>(), GetComponent<Collider>());
+        StartCoroutine(DestroySplinters());
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    public IEnumerator DestroySplinters()
+    {
+        yield return new WaitForSeconds(4.0f);
+        Destroy(gameObject);
+    }
+}
---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/Combat/Explode.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explode : MonoBehaviour {
+
+    public GameObject splintersPrefab;
+
+    void OnDestroy()
+    {
+        Instantiate(splintersPrefab, transform.position, transform.rotation);
+    }
+
+}
