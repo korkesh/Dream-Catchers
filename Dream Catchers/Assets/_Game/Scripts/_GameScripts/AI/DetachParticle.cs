b263823a3dc4c65f5b59c5c9d3f98a2bc558a57e
unknown <gallalex@OA-S142-01.shernet.sheridancollege.ca>
2016-08-11 15:26:23 -0400

    disappearing puff for exploding enemies

---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/AI/DetachParticle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetachParticle : MonoBehaviour {
+
+    public GameObject detach;
+
+    void OnDestroy()
+    {
+        detach.transform.parent = null;
+        detach.SetActive(true);
+    }
+}
