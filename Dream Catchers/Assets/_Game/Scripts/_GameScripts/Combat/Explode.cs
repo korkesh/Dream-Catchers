5b5fb9d430599d88f35511c5e78e5c6bc98f64ab
korkesh <matt.j.correia@gmail.com>
2016-08-12 11:38:14 -0400

    Merge Fix

--- Dream Catchers/Assets/_Game/Scripts/_GameScripts/Combat/Explode.cs
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/Combat/Explode.cs
@@ -4,23 +4,10 @@ using System.Collections;
 public class Explode : MonoBehaviour {
 
     public GameObject SplinterPrefab;
-    public HealthManager HM;
 
     public void explosion()
     {
-        if(HM == null)
-        {
-           HM = this.GetComponent<HealthManager>();
-        }
-
-        if(HM != null)
-        {
-            if(HM.currentHealth <= 0)
-            {
-                Instantiate(SplinterPrefab, transform.position, transform.rotation);
-            }
-        }
-        
+        Instantiate(SplinterPrefab, transform.position, transform.rotation);
     }
 
 }
