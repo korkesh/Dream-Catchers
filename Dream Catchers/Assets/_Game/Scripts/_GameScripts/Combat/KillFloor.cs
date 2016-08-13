53c4810dbf07da33214a2b428693f1d9c616bf05
korkesh <matt.j.correia@gmail.com>
2016-08-13 16:26:44 -0400

    Updated restart time on falling death

--- Dream Catchers/Assets/_Game/Scripts/_GameScripts/Combat/KillFloor.cs
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/Combat/KillFloor.cs
@@ -8,11 +8,23 @@ using System.Collections;
 
 public class KillFloor : MonoBehaviour {
 
+    public float restartDelay = 1.0f;
+
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
             Character_Manager.instance.takeDamage(Character_Manager.instance.maxHealth);
+            StartCoroutine(Restart());
+
         }
     }
+
+    public IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(restartDelay);
+
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCombat>().GameOver();
+    }
+
 }
