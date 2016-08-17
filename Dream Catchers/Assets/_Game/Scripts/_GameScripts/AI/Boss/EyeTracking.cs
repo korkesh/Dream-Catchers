89c49ad969751385f39024807b038ef3d005f4a6
korkesh <matt.j.correia@gmail.com>
2016-08-17 13:57:55 -0400

    eye tracking, boss music

---
+++ Dream Catchers/Assets/_Game/Scripts/_GameScripts/AI/Boss/EyeTracking.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeTracking : MonoBehaviour {
+
+    public GameObject playerTracker;
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(playerTracker)
+        {
+            transform.LookAt(playerTracker.transform, playerTracker.transform.up);
+        }
+
+	}
+}
